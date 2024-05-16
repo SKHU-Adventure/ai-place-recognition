62bf4f9c64362a8faeec1031c521d13a844bfdef
Mujae <phs514525@nate.com>
2024-05-16 17:34:24 +0900

    불필요한 코드 수정'

--- datasets/nordland.py
+++ datasets/nordland.py
@@ -16,6 +16,7 @@ class Nordland(torch.utils.data.Dataset):
         self.img_w = config.img_w
         self.batch_size = config.batch_size
         self.seed = config.seed
+        self.loss = config.loss
         self.data_path = data_path
         self.transform = transforms.Compose([
             transforms.Resize((self.img_h, self.img_w)),
@@ -39,18 +40,23 @@ class Nordland(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         anc_path = self.images[idx]
         pos_path = self.get_positive(anc_path)
-        neg_path = self.get_negative(anc_path)
-        another_neg_path = self.get_another_negative(anc_path, neg_path) 
+        neg_path = self.get_negative(anc_path,0)
+        
         anc = Image.open(anc_path).convert('RGB')
         pos = Image.open(pos_path).convert('RGB')
         neg = Image.open(neg_path).convert('RGB')
-        another_neg = Image.open(another_neg_path).convert('RGB')
+        
         anc = self.transform(anc)
         pos = self.transform(pos)
         neg = self.transform(neg)
-        another_neg = self.transform(another_neg)
 
-        return anc, pos, neg, another_neg
+        if self.loss=="quadruplet":
+            another_neg_path = self.get_negative(anc_path, neg_path)
+            another_neg = Image.open(another_neg_path).convert('RGB')
+            another_neg = self.transform(another_neg)
+            return anc, pos, neg, another_neg
+        else:
+            return anc, pos, neg
 
     def get_positive(self, path):
         season_src = path.split("/")[-3]
@@ -69,18 +75,27 @@ class Nordland(torch.utils.data.Dataset):
 
         return path
 
-    def get_negative(self, path):
+    def get_negative(self, path, neg_path):
         season_src = path.split("/")[-3]
         section_src = path.split("/")[-2]
         number_src = int(os.path.splitext(path.split("/")[-1])[0])
         ext = os.path.splitext(path.split("/")[-1])[1]
 
+        if neg_path!=0:
+            first_neg_season = neg_path.split("/")[-3]
+            first_neg_section = neg_path.split("/")[-2]
+            first_neg_number = int(os.path.splitext(neg_path.split("/")[-1])[0])
         while True:
             season_dst = random.choice(self.seasons)[1]
             section_dst = random.choice(self.sections)[1]
             number_dst = random.randrange(self.maxnum[section_dst][0], self.maxnum[section_dst][1]+1)
-            if season_src != season_dst or section_src != section_dst or abs(number_src - number_dst) > self.window:
-                break
+            if neg_path==0:
+                if season_src != season_dst or section_src != section_dst or abs(number_src - number_dst) > self.window:
+                    break
+            else:
+                if ((season_src != season_dst or section_src != section_dst or abs(number_src - number_dst) > self.window) and
+                    (first_neg_season != season_dst or first_neg_section != section_dst or abs(first_neg_number-number_dst)>self.window)):
+                    break
 
         path = path.replace(season_src, season_dst)
         path = path.replace(section_src, section_dst)
@@ -88,29 +103,3 @@ class Nordland(torch.utils.data.Dataset):
 
         return path
     
-    def get_another_negative(self, path, first_neg_path):
-        season_src = path.split("/")[-3]
-        section_src = path.split("/")[-2]
-        number_src = int(os.path.splitext(path.split("/")[-1])[0])
-        ext = os.path.splitext(path.split("/")[-1])[1]
-
-        # 첫 번째 negative 이미지의 정보 추출
-        first_neg_season = first_neg_path.split("/")[-3]
-        first_neg_section = first_neg_path.split("/")[-2]
-        first_neg_number = int(os.path.splitext(first_neg_path.split("/")[-1])[0])
-
-        while True:
-            season_dst = random.choice(self.seasons)[1]
-            section_dst = random.choice(self.sections)[1]
-            number_dst = random.randrange(self.maxnum[section_dst][0], self.maxnum[section_dst][1] + 1)
-
-            # 첫 번째 negative와 다르고 anchor와도 충분히 다른지 확인
-            if ((season_src != season_dst or section_src != section_dst or abs(number_src - number_dst) > self.window) and
-                (first_neg_season != season_dst or first_neg_section != section_dst or first_neg_number != number_dst)):
-                break
-
-        path = path.replace(season_src, season_dst)
-        path = path.replace(section_src, section_dst)
-        path = path.replace(str(number_src)+ext, str(number_dst)+ext)
-
-        return path
