a7f719250205ff0283cd87161ffff8809fadea0f
Mujae <phs514525@nate.com>
2024-07-08 19:17:10 +0900

    add new version train.py

--- train.py
+++ train.py
@@ -1,34 +1,18 @@
 import os
 import lightning.pytorch as pl
-from pytorch_lightning.loggers import TensorBoardLogger
-import matplotlib.pyplot as plt
+
 from setup import config
 from utils.util_model import LightningTripletNet
 from utils.util_dataset import LightningDataModule
-import pandas as pd
+from utils.util_lightning import get_callbacks, get_logger
+
 
 def main():
 
     dataset = LightningDataModule(config)
     triplet_net = LightningTripletNet(config)
-    
-    tqdm_cb = pl.callbacks.TQDMProgressBar()
-    ckpt_cb = pl.callbacks.ModelCheckpoint(
-        filename="{epoch:02d}_",
-        monitor="val_loss",
-        mode="min",
-        save_top_k=1,
-        verbose=True
-    )
-    early_stopping_cb = pl.callbacks.EarlyStopping(
-        monitor="val_loss",
-        patience=3,
-        mode="min"
-    )
-    lr_monitor_cb = pl.callbacks.LearningRateMonitor(logging_interval='epoch')
-
-    callbacks = [tqdm_cb, ckpt_cb, early_stopping_cb, lr_monitor_cb]
-    logger = TensorBoardLogger(save_dir=os.path.join(config.base_dir, "lightning_logs"), name='triplet_net')
+    callbacks = get_callbacks(config)
+    logger = get_logger(config)
 
     trainer = pl.Trainer(accelerator="gpu",
                         devices=config.gpu_ids,
