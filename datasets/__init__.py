bdedc25ade5c95eca271a466b4a9a33c1336ed6b
Mujae <phs514525@nate.com>
2024-09-11 23:09:21 +0900

    first commit for skhu2

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -5,8 +5,11 @@ def get_dataset(dataset, **kwargs):
     if dataset == 'tokyo':
         from .tokyo import Tokyo
         return Tokyo(**kwargs)
-    if dataset == 'skhu':
-        from .skhu import SKHU
-        return SKHU(**kwargs)
+    if dataset == 'skhu1':
+        from .skhu1 import SKHU1
+        return SKHU1(**kwargs)
+    if dataset == 'skhu2':
+        from .skhu2 import SKHU2
+        return SKHU2(**kwargs)
     else:
         raise ValueError(f"Unknown dataset: {dataset}")
