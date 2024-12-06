5fa7384a22c0d8557a930e3bb3489bf7bee3a83a
Mujae <38150072+Mujae@users.noreply.github.com>
2024-12-06 22:35:11 +0900

    feat: add code for finetuning

--- backbones/__init__.py
+++ backbones/__init__.py
@@ -5,11 +5,12 @@ import torch.nn as nn
 def get_backbone(backbone, **kwargs):
     if backbone in ['vgg16', 'vgg19']:
         pretrained = getattr(importlib.import_module('torchvision.models'), backbone)(weights='DEFAULT').features
-        for param in pretrained.parameters():
-            param.requires_grad = False
         return nn.Sequential(*list(pretrained.children())[:-1])
     elif backbone in ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']:
         pretrained = getattr(importlib.import_module('torchvision.models'), backbone)(weights='DEFAULT')
         for param in pretrained.parameters():
             param.requires_grad = False
+        for param in pretrained.layer4.parameters():
+            param.requires_grad = True
+        
         return nn.Sequential(*list(pretrained.children())[:-1])
